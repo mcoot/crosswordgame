d302b2c3996d316c9440e49f3942dda232636442
Joseph Spearritt <joe.spearritt@live.com>
2023-02-25 22:15:42 +1100

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello there');
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.use('/', (req: Request, res: Response) => {
+  res.send('Hello there :)');
+});
+
+app.listen(8080, () => console.log('Express started on port 8080'));
