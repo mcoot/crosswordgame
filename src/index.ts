a30ad219469db49574ff85ea371b6c56e193f843
Joseph Spearritt <joe.spearritt@live.com>
2023-02-26 11:07:35 +1100

    Add websockets skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+import expressWs from 'express-ws';
 
-const app: Application = express();
+const expressWsInstance = expressWs(express());
+const app = expressWsInstance.app;
 
-app.use('/', (req: Request, res: Response) => {
-  res.send('Hello there :)');
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello there :)\n');
+});
+
+app.ws('/ws', (ws, req) => {
+  ws.on('message', (msg) => {
+    console.log(`Got message "${msg}"`);
+    ws.send('Thanks for ur message');
+  });
 });
 
 app.listen(8080, () => console.log('Express started on port 8080'));
